b1cde5a7552e9b5918200cbd9c7bb8fa731df7c5
Jan Kelemen <jkeleme@gmail.com>
2020-11-18 20:31:35 +0100

    Adapt opera/fixhtml5.sh for Linux Mint 20
    - Chrome is now apparently installed with snap only
    - APT packages chromium-codecs-ffmpeg-extra and chromium-codecs-ffmpeg
      no longer do anything
    - snapd needs to be enabled and installed
      https://www.itzgeek.com/how-tos/linux/linux-mint-how-tos/how-to-enable-snap-package-manager-on-linux-mint-20.html
    - chromium-ffmpeg snap as well `snap install chromium-ffmpeg`
    - This changes the location of libffmpeg.so to the new one which is
      riddled with bunch of folders with different numbers, assumption is
      that the highest number means newest

--- opera/fixhtml5.sh
+++ opera/fixhtml5.sh
@@ -1,1 +1,1 @@
-sudo ln -f /usr/lib/chromium-browser/libffmpeg.so /usr/lib/x86_64-linux-gnu/opera/libffmpeg.so
+sudo ln -f -s /snap/chromium-ffmpeg/current/$( ls /snap/chromium-ffmpeg/current/ | grep chromium-ffmpeg | tail -1)/chromium-ffmpeg/libffmpeg.so /usr/lib/x86_64-linux-gnu/opera/libffmpeg.so
