5febee08588c1083f8b1465d262c8c8a513931c0
Jan Kelemen <jkeleme@gmail.com>
2024-02-27 21:37:13 +0100

    General cleanup
    - Update license dates
    - Remove opera, supereseeded by https://github.com/Ld-Hagen/fix-opera-linux-ffmpeg-widevine

--- opera/fixhtml5.sh
+++
@@ -1,1 +0,0 @@
-sudo ln -f -s /snap/chromium-ffmpeg/current/$( ls /snap/chromium-ffmpeg/current/ | grep chromium-ffmpeg | tail -1)/chromium-ffmpeg/libffmpeg.so /usr/lib/x86_64-linux-gnu/opera/libffmpeg.so
